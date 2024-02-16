5e5a9020ca643661c04c5217c32d2e4a29425187
KABOURI Mohamed Yassine <kabouri.yassine@hotmail.com>
2024-02-16 16:07:09 +0100

    Adding first problem

---
+++ graph/clone_graph.py
@@ -0,0 +1,24 @@
+# Link: https://leetcode.com/problems/clone-graph/description/
+
+from typing import Optional
+class Solution:
+    # I want to implement a simple BFS
+    # We can implement a DFS
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return
+        graph = Node(val=node.val)
+        stack = []
+        stack.append(node)
+
+        mapping = {}
+        mapping[node] = graph
+        while stack != []:
+            current_node = stack.pop()
+            if current_node.neighbors is not None:
+                for neighbor in current_node.neighbors:
+                    if neighbor not in mapping:
+                        mapping[neighbor] = Node(neighbor.val)
+                        stack.append(neighbor)
+                    mapping[current_node].neighbors.append(mapping[neighbor])
+        return graph
